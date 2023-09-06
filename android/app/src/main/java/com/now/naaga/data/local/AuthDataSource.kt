7243bad29f181b39aef4e2bf3e20c29bf00efd94
zillionme <100172683+zillionme@users.noreply.github.com>
2023-09-06 00:56:07 +0000

    Delete android directory

--- android/app/src/main/java/com/now/naaga/data/local/AuthDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.now.naaga.data.local
-
-interface AuthDataSource {
-    fun getAccessToken(): String?
-    fun setAccessToken(newToken: String)
-    fun getRefreshToken(): String?
-    fun setRefreshToken(newToken: String)
-}
--- android/domain/src/main/java/com/now/domain/repository/StatisticsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.now.domain.repository
-
-import com.now.domain.model.Statistics
-
-interface StatisticsRepository {
-    fun getMyStatistics(callback: (Result<Statistics>) -> Unit)
-}
